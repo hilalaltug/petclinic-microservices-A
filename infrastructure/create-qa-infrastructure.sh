662ecc99ec92d63cb125ab1d717789aa4483b611
hilalaltug <hilaltokmak65@gmail.com>
2022-06-29 08:31:00 +0000

    updated key name

--- infrastructure/create-qa-infrastructure.sh
+++ infrastructure/create-qa-infrastructure.sh
@@ -1,6 +1,6 @@
 PATH="$PATH:/usr/local/bin"
 APP_NAME="petclinic"
-ANS_KEYPAIR="aksam-${APP_NAME}-qa.key"
+ANS_KEYPAIR="matt-${APP_NAME}-qa.key"
 AWS_REGION="us-east-1"
 cd infrastructure/qa-k8s-terraform
 terraform init
